e65f1a37ac61e464a8c19f90e5962009b751df94
origin <git@github.com:OljaKn/job4j_design.git>
2025-12-15 15:37:18 +0300

    Разбор первой программы [#505560]

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"job4j.ru/go-lang-base/internal/base"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	first := 100
+	second := 10
+	res := base.Max(first, second)
+	fmt.Println(fmt.Sprintf("%d + %d = %d", first, second, res))
 }
--- internal/base/calcalator.go
+++ internal/base/calcalator.go
@@ -3,3 +3,10 @@ package base
 func Add(first, second int) int {
 	return first + second
 }
+
+func Max(first, second int) int {
+	if first > second {
+		return first
+	}
+	return second
+}

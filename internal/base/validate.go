77a2c7bf5a993ae11afa98f3692ecbada31c4ff3
origin <git@github.com:OljaKn/job4j_design.git>
2025-12-20 14:20:38 +0300

    Типы данных | Struct [#505561 #556344]

--- internal/base/validate.go
+++ internal/base/validate.go
@@ -10,20 +10,19 @@ func Validate(req *ValidateRequest) []string {
 	res := make([]string, 0)
 	if req == nil {
 		res = append(res, "Запрос не должен быть пустым")
+		return res
 	}
-	if req != nil {
-		if req.UserID != "" && req.Title != "" && req.Description != "" {
-			res = append(res, "Запрос корректный!")
-		}
-		if req.UserID == "" {
-			res = append(res, "Id не заполнен")
-		}
-		if req.Title == "" {
-			res = append(res, "Не указано имя запроса")
-		}
-		if req.Description == "" {
-			res = append(res, "Заполните описание запроса")
-		}
+	if req.UserID != "" && req.Title != "" && req.Description != "" {
+		res = append(res, "Запрос корректный!")
+	}
+	if req.UserID == "" {
+		res = append(res, "Id не заполнен")
+	}
+	if req.Title == "" {
+		res = append(res, "Не указано имя запроса")
+	}
+	if req.Description == "" {
+		res = append(res, "Заполните описание запроса")
 	}
 	return res
 }

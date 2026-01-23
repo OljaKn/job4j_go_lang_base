f529350b2ef8bdee7877e5dd8e5da9ee9050de16
origin <git@github.com:OljaKn/job4j_design.git>
2026-01-23 19:52:52 +0300

    tests

--- internal/base/cache.go
+++ internal/base/cache.go
@@ -8,89 +8,90 @@ type Node struct {
 }
 
 type LruCache struct {
-	capacity map[string]*Node
-	size     int
-	Head     *Node
-	Tail     *Node
+	count int
+	size  int
+	Head  *Node
+	Tail  *Node
 }
 
 func NewLruCache(size int) *LruCache {
 	return &LruCache{
-		size:     size,
-		capacity: make(map[string]*Node),
+		size:  size,
+		count: 0,
 	}
 }
 
 func (l *LruCache) Put(key string, value string) {
-	count := len(l.capacity)
-	existsNode := l.capacity[key] // есть ли уже такой ключ
-	if existsNode != nil {        //да - обновляем и переносим в начало
-		existsNode.Value = value
-		if existsNode != l.Head { // нода не в начале кэша
-			if existsNode.Next != nil {
-				existsNode.Next.Prev = existsNode.Prev
+	existsNode := l.Head
+	for existsNode != nil { // ищем ключ
+		if existsNode.Key == key {
+			existsNode.Value = value
+			if existsNode != l.Head {
+				if existsNode.Next != nil {
+					existsNode.Next.Prev = existsNode.Prev
+				}
+				if existsNode.Prev != nil {
+					existsNode.Prev.Next = existsNode.Next
+				}
+				if existsNode == l.Tail {
+					l.Tail = existsNode.Prev
+				}
+				existsNode.Next = l.Head
+				existsNode.Prev = nil
+				l.Head = existsNode
 			}
-			if existsNode.Prev != nil {
-				existsNode.Prev.Next = existsNode.Next
-			}
-			if existsNode == l.Tail {
-				l.Tail = existsNode.Prev
-			}
-			existsNode.Next = l.Head
-			existsNode.Prev = nil
-			l.Head = existsNode
+			return
 		}
-		l.capacity[key] = existsNode // обновляею в мапе или не надо?
-		return
+		existsNode = existsNode.Next
 	}
-	if count >= l.size { // переполнился
-		tailKey := l.Tail.Key
+
+	if l.count >= l.size {
 		if l.Tail.Prev != nil {
 			l.Tail.Prev.Next = nil
 			l.Tail = l.Tail.Prev
 		} else {
-			l.Tail = nil
 			l.Head = nil
-			delete(l.capacity, tailKey)
+			l.Tail = nil
 		}
+		l.count--
 	}
-	newNode := &Node{
-		Key:   key,
-		Value: value,
-	}
+
+	newNode := &Node{Key: key, Value: value}
 	if l.Head == nil {
 		l.Head = newNode
 		l.Tail = newNode
-		l.capacity[key] = newNode
 	} else {
 		newNode.Next = l.Head
 		l.Head.Prev = newNode
 		newNode.Prev = nil
 		l.Head = newNode
-		l.capacity[key] = newNode
 	}
+	l.count++
 }
 
 func (l *LruCache) Get(key string) *string {
-	value := l.capacity[key]
-	if value != nil { //есть нода
-		if value == l.Head {
-			return &value.Value
+	firstNode := l.Head
+	for firstNode != nil {
+		if firstNode.Key == key {
+			if firstNode == l.Head {
+				return &firstNode.Value
+			}
+			if firstNode == l.Tail {
+				l.Tail = firstNode.Prev
+			}
+			if firstNode.Next != nil {
+				firstNode.Next.Prev = firstNode.Prev
+			}
+			if firstNode.Prev != nil {
+				firstNode.Prev.Next = firstNode.Next
+			}
+			firstNode.Next = l.Head
+			firstNode.Prev = nil
+			l.Head.Prev = firstNode
+			l.Head = firstNode
+			return &firstNode.Value
 		}
-		if value == l.Tail {
-			l.Tail = value.Prev
-		}
-		if value.Next != nil {
-			value.Next.Prev = value.Prev
-		}
-		if value.Prev != nil {
-			value.Prev.Next = value.Next
-		}
-		value.Next = l.Head
-		value.Prev = nil
-		l.Head.Prev = value
-		l.Head = value
-		return &value.Value
+		firstNode = firstNode.Next
 	}
 	return nil
 }
--- internal/base/cache_test.go
+++ internal/base/cache_test.go
@@ -14,6 +14,50 @@ func Test_LruCache_Methods(t *testing.T) {
 		cache := base.NewLruCache(3)
 		cache.Put("hello", "world")
 		res := cache.Get("hello")
-		assert.Equal(t, res, "world")
+		assert.Equal(t, "world", *res)
+	})
+
+	t.Run("Node is not exists", func(t *testing.T) {
+		cache := base.NewLruCache(3)
+		cache.Put("hello", "world")
+		cache.Put("one", "two")
+		cache.Put("two", "three")
+		res := cache.Get("three")
+		assert.Nil(t, res)
+	})
+
+	t.Run("Cache size = 1", func(t *testing.T) {
+		cache := base.NewLruCache(1)
+		cache.Put("hello", "world")
+		res := cache.Get("hello")
+		assert.Equal(t, "world", *res)
+	})
+
+	t.Run("Put exsisting node", func(t *testing.T) {
+		cache := base.NewLruCache(3)
+		cache.Put("hello", "world")
+		cache.Put("one", "two")
+		cache.Put("hello", "1111")
+		res := cache.Get("hello")
+		assert.NotEqual(t, "world", *res)
+	})
+
+	t.Run("Capacity > size", func(t *testing.T) {
+		cache := base.NewLruCache(2)
+		cache.Put("hello", "world")
+		cache.Put("one", "two")
+		cache.Put("key", "1111")
+		res := cache.Get("hello")
+		assert.Nil(t, res)
+	})
+
+	t.Run("Cache size = 1, put exists node", func(t *testing.T) {
+		cache := base.NewLruCache(1)
+		cache.Put("hello", "world")
+		cache.Put("two", "value")
+		res := cache.Get("hello")
+		res2 := cache.Get("two")
+		assert.Nil(t, res)
+		assert.Equal(t, "value", *res2)
 	})
 }

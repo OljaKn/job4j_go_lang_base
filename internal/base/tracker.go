b99f33d5c434a426111a52096dcd8dbd1d07e65d
origin <git@github.com:OljaKn/job4j_design.git>
2026-01-24 15:14:10 +0300

    Инкапсуляция [#505565]

--- internal/base/tracker.go
+++ internal/base/tracker.go
@@ -1,11 +1,5 @@
 package base
 
-import (
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-)
-
 type Tracker struct {
 	items []Item
 }
@@ -27,26 +21,4 @@ func (t *Tracker) GetItems() []Item {
 	res := make([]Item, len(t.items))
 	copy(res, t.items)
 	return res
-}
-
-func Test_NewTracker(t *testing.T) {
-	t.Parallel()
-	t.Run("check link leak", func(t *testing.T) {
-		t.Parallel()
-
-		tracker := NewTracker()
-		item := Item{
-			ID:   "1",
-			Name: "First Item",
-		}
-		tracker.AddItem(item)
-
-		res := tracker.GetItems()
-		res[0].Name = "Second Item"
-
-		assert.Equal(t,
-			[]Item{item},
-			tracker.GetItems(),
-		)
-	})
 }

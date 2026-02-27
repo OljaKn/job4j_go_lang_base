ea0f0d2fabb40a763de042c145447acb1778ef65
origin <git@github.com:OljaKn/job4j_design.git>
2026-02-27 16:14:25 +0300

    База данных | Pgx | Goose [#505568]

--- internal/tracker/usecase.go
+++ internal/tracker/usecase.go
@@ -1,42 +1,85 @@
 package tracker
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/google/uuid"
 )
 
+type Input interface {
+	Get() string
+}
+
+type Output interface {
+	Out(string)
+}
+
+type Store interface {
+	Create(ctx context.Context, item Item) error
+	List(ctx context.Context) ([]Item, error)
+	Get(ctx context.Context, id string) (Item, error)
+	Update(ctx context.Context, item Item) error
+	Delete(ctx context.Context, id string) error
+	FindByPartName(ctx context.Context, part string) ([]Item, error)
+}
+
 type Usecase interface {
-	Done(in Input, out Output, tracker *Tracker)
+	Done(ctx context.Context, in Input, out Output, store Store) error
 }
 
 type AddUsecase struct{}
 
-func (u AddUsecase) Done(in Input, out Output, tracker *Tracker) {
+func (u AddUsecase) Done(
+	ctx context.Context,
+	in Input,
+	out Output,
+	store Store,
+) error {
 	out.Out("enter name:")
 	name := in.Get()
 	id := uuid.New().String()
-	_, err := tracker.AddItem(Item{Name: name, ID: id})
-	if err != nil {
-		out.Out("failed add item")
+
+	if err := store.Create(
+		ctx,
+		Item{ID: id, Name: name},
+	); err != nil {
+		return fmt.Errorf("failed to create item: %w", err)
 	}
+	return nil
 }
 
 type GetUsecase struct{}
 
-func (u GetUsecase) Done(_ Input, out Output, tracker *Tracker) {
-	for _, item := range tracker.items {
-		out.Out(item.toString())
+func (u GetUsecase) Done(
+	ctx context.Context,
+	in Input,
+	out Output,
+	store Store,
+) error {
+	items, err := store.List(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get items: %w", err)
 	}
+	for _, item := range items {
+		out.Out(item.ID + " " + item.Name)
+	}
+	return nil
 }
 
 type UpdateUsecase struct{}
 
-func (u UpdateUsecase) Done(in Input, out Output, tracker *Tracker) {
+func (u UpdateUsecase) Done(
+	ctx context.Context,
+	in Input,
+	out Output,
+	store Store,
+) error {
 	out.Out("enter ID for update:")
 	id := in.Get()
-	_, err := tracker.FindById(id)
+	_, err := store.Get(ctx, id)
 	if err != nil {
-		out.Out("Not found Item")
-		return
+		return fmt.Errorf("not found Item with id: %w", err)
 	}
 	out.Out("Enter new name:")
 	newName := in.Get()
@@ -44,34 +87,44 @@ func (u UpdateUsecase) Done(in Input, out Output, tracker *Tracker) {
 		ID:   id,
 		Name: newName,
 	}
-	if tracker.UpdateItems(updateItem) == nil {
+	if store.Update(ctx, updateItem) == nil {
 		out.Out("Access")
 	}
+	return nil
 }
 
 type DeleteUsecase struct{}
 
-func (u DeleteUsecase) Done(in Input, out Output, tracker *Tracker) {
+func (u DeleteUsecase) Done(ctx context.Context,
+	in Input,
+	out Output,
+	store Store,
+) error {
 	out.Out("Enter Id for delete:")
 	id := in.Get()
-	res := tracker.DeleteItems(id)
-	if res == true {
-		out.Out("Access")
-	} else {
-		out.Out("Not found Item")
+	err := store.Delete(ctx, id)
+	if err != nil {
+		return fmt.Errorf("not found Item for delete with id: %w", err)
 	}
+	out.Out("Access")
+	return nil
 }
 
 type FindByNameUsecase struct{}
 
-func (u FindByNameUsecase) Done(in Input, out Output, tracker *Tracker) {
+func (u FindByNameUsecase) Done(ctx context.Context,
+	in Input,
+	out Output,
+	store Store,
+) error {
 	out.Out("Enter string for found:")
 	part := in.Get()
-	res := tracker.FindByPartName(part)
-	if len(res) == 0 {
+	items, err := store.FindByPartName(ctx, part)
+	if err != nil {
 		out.Out("Not found Item")
 	}
-	for _, item := range res {
-		out.Out(item.toString())
+	for _, item := range items {
+		out.Out(item.ID + " " + item.Name)
 	}
+	return nil
 }

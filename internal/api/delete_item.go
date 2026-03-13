87a6829f9ab990a45b14c59369c903ce5e9e2194
origin <git@github.com:OljaKn/job4j_design.git>
2026-03-13 14:40:34 +0300

    Микросервис | Fiber [#505569 #559645]

--- internal/api/delete_item.go
+++ internal/api/delete_item.go
@@ -7,11 +7,13 @@ import (
 
 func (s *Server) DeleteItem(c *fiber.Ctx) error {
 	id := c.Params("id")
+	if id == "" {
+		return fiber.NewError(fiber.StatusBadRequest, "id is required")
+	}
 	err := s.Repository.Delete(c.Context(), id)
 	if err != nil {
 		log.Errorw("s.Repository.Delete", err)
 		return fiber.NewError(fiber.StatusInternalServerError, "internal server error")
 	}
 	return c.Status(fiber.StatusNoContent).Send(nil)
-	//return c.Status(fiber.StatusCreated).JSON(fiber.Map{})
 }
